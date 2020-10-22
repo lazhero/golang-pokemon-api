f3a287731649f1a53670d8e0a31ede64e068c547
Luis Zuniga <luisandreyzuniga@gmail.com>
2020-10-22 12:45:14 -0600

    Changing the port to the app

--- main.go
+++ main.go
@@ -7,38 +7,45 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"pokemon-api/database"
 )
+
+func addNewPokemon(w http.ResponseWriter, r *http.Request) {
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var pokemon database.Pokemon
+
+	json.Unmarshal(requestBody, &pokemon)
+	found := false
+	for i := 0; i < len(database.PokemonDb); i++ {
+		if database.PokemonDb[i] == pokemon {
+			found = true
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+	}
+	if !found {
+		database.PokemonDb = append(database.PokemonDb, pokemon)
+	}
+	w.WriteHeader(http.StatusOK)
+}
 
 func getAllPokemons(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(database.PokemonDb)
 }
 
 func handleRequests() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "0"
+	}
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
-	myRouter.HandleFunc("/addNewPokemon", addNewPokemon).Methods("POST")
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	myRouter.HandleFunc("/pokemons", addNewPokemon).Methods("POST")
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
-func addNewPokemon(w http.ResponseWriter, r *http.Request) {
-	requestBody,_ := ioutil.ReadAll(r.Body)
-	var pokemon database.Pokemon
-	err := json.Unmarshal(requestBody, &pokemon)
-	if err==nil{
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	for i := 0; i < len(database.PokemonDb); i++ {
-		if database.PokemonDb[i].ID == pokemon.ID {
-
-			w.WriteHeader(http.StatusNotModified)
-			return
-		}
-	}
-	database.PokemonDb = append(database.PokemonDb, pokemon)
-	w.WriteHeader(http.StatusOK)
-}
 func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
@@ -47,6 +54,7 @@ func commonMiddleware(next http.Handler) http.Handler {
 }
 
 func main() {
+	//
 	fmt.Println("Pokemon Rest API")
 	handleRequests()
 }
