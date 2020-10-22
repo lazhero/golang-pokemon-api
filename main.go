9636522261dd6597bef6be2487afdff295ebc835
adrian <adriantec2019@gmail.com>
2020-10-21 22:24:22 -0600

    post capability

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"pokemon-api/database"
+
+	"github.com/gorilla/mux"
 )
 
 func getAllPokemons(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +19,19 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
+	myRouter.HandleFunc("/pokemons", addNewPokemon).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
+func addNewPokemon(w http.ResponseWriter, r *http.Request) {
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article database.Pokemon
+	json.Unmarshal(reqBody, &article)
+	database.PokemonDb = append(database.PokemonDb, article)
+	json.NewEncoder(w).Encode(article)
+
+}
 func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
