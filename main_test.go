eba299caa4d35df9b918b17d87797c4e621c4eae
adrian <adriantec2019@gmail.com>
2020-10-23 20:13:06 -0600

    jeje, el commit anterior estaba mal

--- main_test.go
+++ main_test.go
@@ -4,22 +4,43 @@ import (
 	"bytes"
 	"math/rand"
 	"net/http"
+	"pokemon-api/database"
 	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func isIn(id string) bool {
+
+	for i := 0; i < len(database.PokemonDb); i++ {
+		if database.PokemonDb[i].ID == id {
+			return true
+		}
+
+	}
+	return false
+}
+
+func TestConnection(t *testing.T) {
+
+	resp, err := http.Get("https://pokemontengoqueatraparlos.herokuapp.com/pokemons")
+	algo := resp == nil
+	if err == nil {
+	}
+	assert.Equal(t, true, algo, "verificando conexion")
+}
+
 func TestAddPok(t *testing.T) {
-	newId := strconv.Itoa(rand.Intn(100))
+	newID := strconv.Itoa(rand.Intn(100))
 	var jsonStr = []byte(`  {
-        "ID":` + newId + `
+        "ID":` + newID + `
         "Name": "Csssharmeleon",
         "Type": "Fire"
 	}`)
 
 	http.NewRequest("POST", "https://pokemontengoqueatraparlos.herokuapp.com/pokemons", bytes.NewBuffer(jsonStr))
 
-	assert.Equal(t, true, isIn(newId), "verificando que se haya agregado")
+	assert.Equal(t, true, isIn(newID), "verificando que se haya agregado")
 
 }
